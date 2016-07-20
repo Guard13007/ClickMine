4d99a1bc3c7c05e4dc259e72f216dec3638693c4
Paul Liverman III <paul.liverman.iii@gmail.com>
2016-07-20 14:16:05 -0700

    testing sending data

--- static/js/game.js
+++ static/js/game.js
@@ -5,7 +5,7 @@ var punch = $("<a>punch a tree</a>").click(function() {
 $(document).ready(function() {
     $("#do").append("<div id='loading'>loading...</div>")
     // send request for data, remove loading, add data
-    $.post("https://clickmine.guard13007.com/get", "{\"fuck\":\"truesday\"}", function(data, status) {
+    $.post("https://clickmine.guard13007.com/get", {fuck: "testing"}, function(data, status) {
         console.log(data)
         console.log(status)
     })
